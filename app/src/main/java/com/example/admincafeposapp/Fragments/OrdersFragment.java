56f2bacd7d45c475e72e7474ad5037fb430be5dd
Sheldon <100086149@students.swinburne.edu.my>
2019-10-03 19:22:10 +0800

    Built the nav bar

---
+++ app/src/main/java/com/example/admincafeposapp/Fragments/OrdersFragment.java
@@ -0,0 +1,23 @@
+package com.example.admincafeposapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.admincafeposapp.R;
+
+public class OrdersFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_orders, container, false);
+    }
+
+}
--- app/src/main/java/com/example/admincafeposapp/MainActivity.java
+++ app/src/main/java/com/example/admincafeposapp/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.admincafeposapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.admincafeposapp.Fragments.MembersFragment;
+import com.example.admincafeposapp.Fragments.MenuFragment;
+import com.example.admincafeposapp.Fragments.OrdersFragment;
+import com.example.admincafeposapp.Fragments.ReservationFragment;
+import com.example.admincafeposapp.Fragments.TablesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_menu:
+                    selectedFragment = new MenuFragment();
+                    break;
+                case R.id.navigation_orders:
+                    selectedFragment = new OrdersFragment();
+                    break;
+                case R.id.navigation_tables:
+                    selectedFragment = new TablesFragment();
+                    break;
+                case R.id.navigation_reservation:
+                    selectedFragment = new ReservationFragment();
+                    break;
+                case R.id.navigation_members:
+                    selectedFragment = new MembersFragment();
+                    break;
+            }
+
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
 }

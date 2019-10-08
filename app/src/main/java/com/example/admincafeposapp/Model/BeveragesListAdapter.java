a3808c113a01d376ec28e40c030790c59d2fecc6
Lester Tang <lestertangjiarui1999@gmail.com>
2019-10-08 22:50:58 +0800

    Pulling from and inserting data to firestore

---
+++ app/src/main/java/com/example/admincafeposapp/Model/BeveragesListAdapter.java
@@ -0,0 +1,56 @@
+package com.example.admincafeposapp.Model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.admincafeposapp.R;
+
+import java.util.List;
+
+public class BeveragesListAdapter extends RecyclerView.Adapter<BeveragesListAdapter.ViewHolder> {
+    private Context context;
+    private List<Beverages> beveragesList;
+
+    public BeveragesListAdapter(Context context, List<Beverages> beveragesList) {
+        this.context = context;
+        this.beveragesList = beveragesList;
+    }
+
+    @Override
+    public BeveragesListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.beverages_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(BeveragesListAdapter.ViewHolder holder, int position) {
+        holder.nameText.setText(beveragesList.get(position).getItem_name());
+        holder.priceText.setText(String.valueOf(beveragesList.get(position).getItem_price()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return beveragesList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        View bView;
+
+        public TextView nameText, priceText;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            bView = itemView;
+
+            nameText = bView.findViewById(R.id.b_name_text);
+            priceText = bView.findViewById(R.id.b_price_text);
+        }
+    }
+}

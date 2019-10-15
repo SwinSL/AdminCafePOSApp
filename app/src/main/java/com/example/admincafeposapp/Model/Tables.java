8ac491b7c16234763bcab1aa60c0b47716345cae
winterkho <tongfile97@gmail.com>
2019-10-15 19:37:37 +0800

    Build table management UI and table class

---
+++ app/src/main/java/com/example/admincafeposapp/Model/Tables.java
@@ -0,0 +1,37 @@
+package com.example.admincafeposapp.Model;
+
+public class Tables {
+
+    String tableNo, tableStatus;
+    int tableNoOfSeat;
+
+    public Tables(String tableNo,int tableNoOfSeat, String tableStatus) {
+        this.tableNo = tableNo;
+        this.tableStatus = tableStatus;
+        this.tableNoOfSeat = tableNoOfSeat;
+    }
+
+    public String getTableNo() {
+        return tableNo;
+    }
+
+    public void setTableNo(String tableNo) {
+        this.tableNo = tableNo;
+    }
+
+    public String getTableStatus() {
+        return tableStatus;
+    }
+
+    public void setTableStatus(String tableStatus) {
+        this.tableStatus = tableStatus;
+    }
+
+    public int getTableNoOfSeat() {
+        return tableNoOfSeat;
+    }
+
+    public void setTableNoOfSeat(int tableNoOfSeat) {
+        this.tableNoOfSeat = tableNoOfSeat;
+    }
+}

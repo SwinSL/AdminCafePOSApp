fdfd9e9d3d2e1d5ed40bc26b4c699d2fd51ffcbc
Kai <chanjunkai99@hotmail.com>
2019-11-09 14:37:43 +0800

    modify transaction model

--- app/src/main/java/com/example/admincafeposapp/Model/Transaction.java
+++ app/src/main/java/com/example/admincafeposapp/Model/Transaction.java
@@ -5,13 +5,15 @@ import java.util.List;
 public class Transaction {
     String date;
     List<String> orderID, sales;
+    List<Boolean> member;
 
     public Transaction() {}
 
-    public Transaction(String date, List<String> orderID, List<String> sales) {
+    public Transaction(String date, List<String> orderID, List<String> sales, List<Boolean> member) {
         this.date = date;
         this.orderID = orderID;
         this.sales = sales;
+        this.member = member;
     }
 
     public String getDate() {
@@ -24,5 +26,9 @@ public class Transaction {
 
     public List<String> getSales() {
         return sales;
+    }
+
+    public List<Boolean> getMember() {
+        return member;
     }
 }

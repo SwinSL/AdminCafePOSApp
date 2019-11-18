90fcaf35ef51f0a5217bda44998e98fa87de15dd
SwinSL <100086149@students.swinburne.edu.my>
2019-11-18 09:53:59 +0800

    Switched login activity to first activity.

--- app/src/main/java/com/example/admincafeposapp/Fragments/TablesFragment.java
+++ app/src/main/java/com/example/admincafeposapp/Fragments/TablesFragment.java
@@ -2,9 +2,7 @@ package com.example.admincafeposapp.Fragments;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,14 +30,11 @@ import com.example.admincafeposapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import org.w3c.dom.Document;
 
 import java.util.ArrayList;
 import java.util.Objects;
